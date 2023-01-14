91bf96a21edf95c0e318abd93daea3d38559bd6c
Scott Yuan <scoworthyuan@gmail.com>
2023-01-14 16:24:59 +1100

    changed initial reading supplier excel file logic, automatically creates a new file if not exist. added readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 #Auto Move Files
 
-The app automatically moves all tax invoices into the correct folder, and automates folder creation.
+This function automatically reads Supplier-Payment-Method.xlsx from the same folder, and move all invoices in the specified path to the correct sub folders.
+
+Supplier-Payment-Method.xlsx has 2 columns, 1 is "Suppliers", another is "Payment Method". The current payment method is determined to "Credit Card Account", "Business Account" or "Home Loan Account".
+
+Add new lines if the supplier is not already pre-defined.
+
+Each reporting quarter are determined as: 1st Quarter: Jan - Mar, 2nd Quarter: Apr - Jun, 3rd Quarter: Jul - Sep, 4th Quarter: Oct - Dec.
--- sort-invoice.py
+++ sort-invoice.py
@@ -6,23 +6,26 @@ import pandas as pd
 
 def move_file():
     """
-    This function automatically reads Supplier-Payment-Method.xlsx from the same folder, and move all invoices
-    in the specified path to the correct sub folders.
+    This function automatically reads Supplier-Payment-Method.xlsx from the same folder, and move
+    all invoices in the specified path to the correct sub folders.
 
-    Supplier-Payment-Method.xlsx has 2 columns, 1 is "Suppliers", another is "Payment Method". The current payment
-    method is determined to "Credit Card Account", "Business Account" or "Home Loan Account".
+    Supplier-Payment-Method.xlsx has 2 columns, 1 is "Suppliers", another is "Payment Method". The
+    current payment method is determined to "Credit Card Account", "Business Account"
+    or "Home Loan Account".
 
     Add new lines if the supplier is not already pre-defined.
 
-    Each reporting quarter are determined as: 1st Quarter: Jan - Mar, 2nd Quarter: Apr - Jun, 3rd Quarter: Jul - Sep,
-    4th Quarter: Oct - Dec.
+    Each reporting quarter are determined as: 1st Quarter: Jan - Mar, 2nd Quarter: Apr - Jun,
+    3rd Quarter: Jul - Sep, 4th Quarter: Oct - Dec.
     """
 
 
     # function setup
     path = "/Users/scott/Library/CloudStorage/OneDrive-Personal/Scott and Coco Pty Ltd/Invoices/"
     supplier_file = 'Supplier-Payment-Method.xlsx'
-    account_folders = ['Westpac Credit Card 0631', 'CBA Business Account 5433', 'Westpac Home Loan 800269']
+    account_folders = ['Westpac Credit Card 0631',
+                       'CBA Business Account 5433',
+                       'Westpac Home Loan 800269']
 
     # basic configuration
     quarter = input('What is the quarter? e.g. q1, q2, q3, q4\n')
@@ -46,8 +49,12 @@ def move_file():
 
     current_year = input('What is the year?e.g. 2023\n')  # year
     #inital setup for supplier payment methods
+    default_df = pd.DataFrame(data=None, columns=['Suppliers', 'Payment Method'])
+
     if not os.path.exists(supplier_file):
-        print(f'Please create "{supplier_file}" and put inside the folder.')
+        default_df.to_excel(supplier_file, index=False)
+        print(f'Created {supplier_file}. Add entries to start.')
+
     df = pd.read_excel(supplier_file)
 
     credit_card_suppliers = df.loc[df['Payment Method'] == 'Credit Card Account']['Suppliers'].to_numpy().tolist()
@@ -71,33 +78,33 @@ def move_file():
             os.makedirs(folder) #make folder recursively
             print(f'Created Folder: {folder}')
 
-    #move files
+    # move files
     for invoice in invoices:
         for credit_card_supplier in credit_card_suppliers:
             if (invoice.lower().find(credit_card_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(credit_card_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == first_month:
-                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(first_month)-1]} {current_year}/Westpac Credit Card 0631/")+invoice)
+                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(first_month)-1]} {current_year}/{account_folders[0]}/")+invoice)
             elif (invoice.lower().find(credit_card_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(credit_card_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == second_month:
-                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(second_month)-1]} {current_year}/Westpac Credit Card 0631/")+invoice)
+                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(second_month)-1]} {current_year}/{account_folders[0]}/")+invoice)
             elif (invoice.lower().find(credit_card_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(credit_card_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == third_month:
-                shutil.move(path + "/" + invoice, os.path.expanduser(f"{path}{the_month[int(third_month)-1]} {current_year}/Westpac Credit Card 0631/") + invoice)
+                shutil.move(path + "/" + invoice, os.path.expanduser(f"{path}{the_month[int(third_month)-1]} {current_year}/{account_folders[0]}/") + invoice)
 
     # paid by business bank account
         for ba_supplier in ba_suppliers:
             if (invoice.lower().find(ba_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(ba_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == first_month:
-                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(first_month)-1]} {current_year}/CBA Business Account 5433/")+invoice)
+                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(first_month)-1]} {current_year}/{account_folders[1]}/")+invoice)
             elif (invoice.lower().find(ba_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(ba_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == second_month:
-                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(second_month)-1]} {current_year}/CBA Business Account 5433/")+invoice)
+                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(second_month)-1]} {current_year}/{account_folders[1]}/")+invoice)
             elif (invoice.lower().find(ba_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(ba_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == third_month:
-                shutil.move(path + "/" + invoice, os.path.expanduser(f"{path}{the_month[int(third_month)-1]} {current_year}/CBA Business Account 5433/")+invoice)
+                shutil.move(path + "/" + invoice, os.path.expanduser(f"{path}{the_month[int(third_month)-1]} {current_year}/{account_folders[1]}/")+invoice)
 
     # paid by home loan account
         for hl_supplier in hl_suppliers:
             if (invoice.lower().find(hl_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(hl_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == first_month:
-                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(first_month)-1]} {current_year}/Westpac Home Loan 800269/")+invoice)
+                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(first_month)-1]} {current_year}/{account_folders[2]}/")+invoice)
             elif (invoice.lower().find(hl_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(hl_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == second_month:
-                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(second_month)-1]} {current_year}/Westpac Home Loan 800269/")+invoice)
+                shutil.move(path+"/"+invoice, os.path.expanduser(f"{path}{the_month[int(second_month)-1]} {current_year}/{account_folders[2]}/")+invoice)
             elif (invoice.lower().find(hl_supplier.lower()) >= 0 and invoice.split()[-1].split('.')[0][2:4]) or (invoice.lower().find(hl_supplier.lower()) >= 0 and invoice.split()[-2][2:4]) == third_month:
-                shutil.move(path + "/" + invoice, os.path.expanduser(f"{path}{the_month[int(third_month)-1]} {current_year}/Westpac Home Loan 800269/")+invoice)
+                shutil.move(path + "/" + invoice, os.path.expanduser(f"{path}{the_month[int(third_month)-1]} {current_year}/{account_folders[2]}/")+invoice)
 
     print('Files moved to the folders.')
 
